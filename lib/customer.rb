472eb3b5b7512e65f74802fb1663796e33d46994
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2019-10-14 07:34:52 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -46,19 +46,4 @@ class Customer
     oldest_customer
   end
 
-def meals
-  Meal.all.select do |meal|
-    meal.customer == self
-  end
-
-def waiters
-  meals.map do |meal|
-    meal.waiter
-  end
-  
-end
-  
-end
-
-
 end
